a183b36aca8ac1b401ea522cba2e574e3dc335ff
Anas Zakaria <anaszcm@gmail.com>
2019-10-15 12:49:05 +0800

    Added queryString result on routes

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import PageTitle from '../components/PageTitle'
+import queryString from 'query-string'
 
 export default class Dashboard extends Component {
-
     constructor(props) {
         super(props)
         this.state = {
-            id: null
+            id: null,
+            user: null,
+            role: null
         }
     }
 
     componentDidMount() {
         const { match: { params } } = this.props
-        console.log(params)
-        this.setState({ id: params.id })
+        const { location: { search } } = this.props
+        const result = queryString.parse(search)
+        this.setState({ id: params.id, user: result.user, role: result.role })
     }
 
     render() {
@@ -21,8 +24,9 @@ export default class Dashboard extends Component {
             <div>
                 <PageTitle title="Dashboard" />
                 <section style={styles.container}>
-                    <p>This is the dashboard page</p>
                     <p>ID: {this.state.id}</p>
+                    <p>User: {this.state.user}</p>
+                    <p>Role: {this.state.role}</p>
                 </section>
             </div>
         )
