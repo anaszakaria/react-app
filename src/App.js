bb1649d8a94c4a81fc1f2f07defb66822fef6b04
Anas Zakaria <anaszcm@gmail.com>
2019-10-17 15:57:52 +0800

    Redirect to SignIn for protected pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 
 // router
 import routes from 'router/index'
@@ -32,16 +40,15 @@ export default class App extends Component {
                     <LeftPanel />
                     <section style={styles.mainContent}>
                         <Switch>
-                            {
-                                routes.map((route, index) => {
-                                    return <Route
-                                        key={index}
-                                        path={process.env.PUBLIC_URL + route.path}
-                                        exact
-                                        component={route.component}
-                                    />
-                                })
-                            }
+                            <Route path="/public">
+                                <PublicPage />
+                            </Route>
+                            <Route path="/login">
+                                <LoginPage />
+                            </Route>
+                            <PrivateRoute path="/protected">
+                                <ProtectedPage />
+                            </PrivateRoute>
                             <Route render={(props) => <Error404 {...props} title={'Error 404 - Page Not Found'}/>} />
                         </Switch>
                     </section>
@@ -50,6 +57,84 @@ export default class App extends Component {
             </div>
         )
     }
+}
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+function AuthButton() {
+  let history = useHistory();
+
+  return fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome!{" "}
+      <button
+        onClick={() => {
+          fakeAuth.signout(() => history.push("/"));
+        }}
+      >
+        Sign out
+      </button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  );
+}
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
+function ProtectedPage() {
+  return <h3>Protected</h3>;
+}
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
 }
 
 const styles = {
--- src/router/index.js
+++ src/router/index.js
@@ -8,27 +8,32 @@ const routes = [
     {
         path: "/",
         component: Home,
+        access: 'public',
         meta: { showFooter: true }
     },
     {
         path: "/about",
         component: About,
+        access: 'public',
         meta: { showFooter: true }
     },
     {
         path: "/contact",
         component: Contact,
+        access: 'public',
         meta: { showFooter: true }
     },
     {
         path: "/dashboard/:id",
         component: Dashboard,
+        access: 'protected',
         meta: {}
     },
     // USER
     {
         path: "/signin",
         component: SignIn,
+        access: 'public',
         meta: {}
     }
 ]
